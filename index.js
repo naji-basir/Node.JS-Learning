09b1ed41bcd604850e34d69df4cee6b126d7b56d
Basir Naji <basirnaji007@gmail.com>
2025-11-01 18:00:15 +0430

    products connected

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const fs = require("fs");
-const url = require("url");
-const http = require("http");
+const fs = require('fs');
+const url = require('url');
+const http = require('http');
 
 //♦️FILE
 
@@ -23,53 +23,63 @@ const http = require("http");
 
 // // ♦️ SERVER
 // ➡️ create a server
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-const tempOverview = fs.readFileSync(
-  `${__dirname}/templates/template-overview.html`,
-  "utf-8"
-);
-const tempProduct = fs.readFileSync(
-  `${__dirname}/templates/template-product.html`,
-  "utf-8"
-);
-const tempCard = fs.readFileSync(
-  `${__dirname}/templates/template-card.html`,
-  "utf-8"
-);
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  return output;
+};
 
 const server = http.createServer((req, res) => {
-  console.log("👉 New request:", req.url); // log every request
-
-  const pathName = req.url;
+  console.log('👉 New request:', req.url); // log every request
+  console.log(url.parse(req.url, true));
+  const { query, pathname } = url.parse(req.url, true);
 
   // Overview
-  if (pathName === "/" || pathName === "/overview") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    // res.end("<h1>This is the OVERVIEW page.</h1>");
-    res.end(tempOverview);
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('%PRODUCT_CARDS%', cardsHtml);
+    res.end(output);
+
+    // console.log(cardsHtml);
 
     // Product
-  } else if (pathName === "/product") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    // res.end("<h1>This is the PRODUCT page.</h1>");
-    res.end(tempProduct);
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    console.log(query);
+    res.end(output);
 
     //Read The Data API
-  } else if (pathName === "/api") {
+  } else if (pathname === '/api') {
     res.writeHead(200, {
-      "Content-type": "application/json",
-      "my-own-header": "naji",
+      'Content-type': 'application/json',
+      'my-own-header': 'naji',
     });
     res.end(data);
 
     // 404 - Not Found
   } else {
     res.writeHead(404, {
-      "Content-Type": "text/html",
-      "my-own-header": "hello-world-by-naji",
+      'Content-Type': 'text/html',
+      'my-own-header': 'hello-world-by-naji',
     });
     res.end(`
       <!DOCTYPE html>
@@ -83,7 +93,7 @@ const server = http.createServer((req, res) => {
         </head>
         <body>
           <h1>404 - Page Not Found</h1>
-          <p>The page <b>${pathName}</b> does not exist.</p>
+          <p>The page <b>${pathname}</b> does not exist.</p>
         </body>
       </html>
     `);
@@ -91,6 +101,6 @@ const server = http.createServer((req, res) => {
 });
 
 // ➡️ listen to the server
-server.listen(8000, "127.0.0.1", () => {
-  console.log("✅ Listening on http://127.0.0.1:8000");
+server.listen(8000, '127.0.0.1', () => {
+  console.log('✅ Listening on http://127.0.0.1:8000');
 });
