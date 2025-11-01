7e00bf82bd53b9b68f565b2dd50013f00f6e1483
Basir Naji <basirnaji007@gmail.com>
2025-11-01 18:47:45 +0430

    module created and imported

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const url = require('url');
 const http = require('http');
 
+const replaceTemplate = require('./modules/replaceTemplate');
+require('./');
+
 //♦️FILE
 
 // //Blocking, synchronous way
@@ -29,21 +32,6 @@ const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'u
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
-
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  return output;
-};
 
 const server = http.createServer((req, res) => {
   console.log('👉 New request:', req.url); // log every request
